2c11a390e54ec2a6c6862ee28a5be8ad067ed226
Himani Bhatt <Himani.Bhatt@rishabhsoft.com>
2024-04-25 19:02:02 +0530

    MERN chat-app Practice

--- server/lib/helper.js
+++ server/lib/helper.js
@@ -4,10 +4,7 @@ export const getOtherMember = (members, userId) =>
   members.find((member) => member._id.toString() !== userId.toString());
 
 export const getSockets = (users = []) => {
-  const sockets = users.map((user) => {
-    userSocketIDs.get(user.toString())
-    //console.log("userToString", user.toString());
-  });
+  const sockets = users.map((user) => userSocketIDs.get(user.toString()));
   
   return sockets;
   
